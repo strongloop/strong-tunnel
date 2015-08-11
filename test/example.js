97418014676b76f1603cfd0f8e980c7654181d9c
Ryan Graham <ryan@strongloop.com>
2015-08-11 10:11:34 -0700

    test: make example and test-example separate
    This allows test-example to emit proper TAP without modifying the
    example, which is used in README.md.

---
+++ test/example.js
@@ -0,0 +1,44 @@
+var fmt = require('util').format;
+var http = require('http');
+var st = require('../');
+
+var server = http.createServer(function(req, res) {
+  res.end(JSON.stringify(req.headers));
+});
+
+var sshOpts = {
+  host: '127.0.0.1',
+};
+
+server.listen(3030, function() {
+  var direct = 'http://127.0.0.1:3030/';
+  var tunneled = 'http+ssh://127.0.0.1:3030/';
+
+  // Standard request using URL string
+  http.get(direct, resLog('%s using %s:', direct, direct));
+
+  // URL is only modified if a tunnelling URL was given
+  st(direct, function(err, url) {
+    if (err) throw err;
+    // url == direct, unmodified
+    http.get(url, resLog('%s using %s:', direct, url));
+  });
+
+  // optional second argument containing ssh config
+  st(tunneled, sshOpts, function(err, url) {
+    if (err) throw err;
+    // url != tunneled, is modified
+    http.get(url, resLog('%s using %s:', tunneled, url));
+  });
+
+  server.unref();
+});
+
+function resLog(prefix) {
+  prefix = fmt.apply(null, arguments);
+  return function onResponse(res) {
+    res.on('data', function(d) {
+      console.log('%s -> %s', prefix, d);
+    });
+  };
+}
--- test/test-example.js
+++ test/test-example.js
@@ -1,44 +1,11 @@
-var fmt = require('util').format;
-var http = require('http');
-var st = require('../');
+var fork = require('child_process').fork;
+var tap = require('tap');
 
-var server = http.createServer(function(req, res) {
-  res.end(JSON.stringify(req.headers));
-});
-
-var sshOpts = {
-  host: '127.0.0.1',
-};
-
-server.listen(3030, function() {
-  var direct = 'http://127.0.0.1:3030/';
-  var tunneled = 'http+ssh://127.0.0.1:3030/';
-
-  // Standard request using URL string
-  http.get(direct, resLog('%s using %s:', direct, direct));
-
-  // URL is only modified if a tunnelling URL was given
-  st(direct, function(err, url) {
-    if (err) throw err;
-    // url == direct, unmodified
-    http.get(url, resLog('%s using %s:', direct, url));
+tap.test('example script runs', function(t) {
+  var example = fork(require.resolve('./example'));
+  example.on('exit', function(code, signal) {
+    t.assert(!code, 'exit 0');
+    t.assert(!signal, 'not signalled to exit');
+    t.end();
   });
-
-  // optional second argument containing ssh config
-  st(tunneled, sshOpts, function(err, url) {
-    if (err) throw err;
-    // url != tunneled, is modified
-    http.get(url, resLog('%s using %s:', tunneled, url));
-  });
-
-  server.unref();
 });
-
-function resLog(prefix) {
-  prefix = fmt.apply(null, arguments);
-  return function onResponse(res) {
-    res.on('data', function(d) {
-      console.log('%s -> %s', prefix, d);
-    });
-  };
-}
